cc0df70e7aec4f792878771f67c5002105344856
Zahar <Zahar@DESKTOP-0T3OVPN.local>
2024-11-18 05:32:12 +0700

    Добавлена булевая функция IsSorted, проверяющая массив на отсортированность

--- ShellSorting/ShellSorting.cpp
+++ ShellSorting/ShellSorting.cpp
@@ -82,6 +82,18 @@ void ShellHibbard(std::vector<int>& array) {
 }
 
 
+bool isSorted(const std::vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
 
 
 int main() {
