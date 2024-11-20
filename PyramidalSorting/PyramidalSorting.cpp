149ed65427798a2e1a143417c4abafea7b186088
Zahar <Zahar@DESKTOP-0T3OVPN.local>
2024-11-21 01:29:31 +0700

    Реализованы функции isSorted и generateRandomArray из предыдущей лабораторной

---
+++ PyramidalSorting/PyramidalSorting.cpp
@@ -0,0 +1,50 @@
+﻿// PyramidalSorting.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <functional>
+
+
+
+bool isSorted(const std::vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; ++i) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+std::vector<int> generateRandomArray(size_t size, int minValue, int maxValue) {
+    std::vector<int> array(size);
+    std::srand(static_cast<unsigned int>(std::time(0)));
+    for (size_t i = 0; i < size; ++i) {
+        array[i] = minValue + std::rand() % (maxValue - minValue + 1);
+    }
+
+    return array;
+}
+
+
+int main()
+{
+  
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
